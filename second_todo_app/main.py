1a5c5f719971a2d5da618dcd5821d21f7f2d742b
Tatsuya Matoba <tatsuya.matoba.wk.jp@gmail.com>
2025-09-05 01:18:37 +0900

    Jinja2で変数をレンダリング

--- second_todo_app/main.py
+++ second_todo_app/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from datetime import datetime
 
 app = FastAPI()
 templates = Jinja2Templates(directory="second_todo_app/templates")
+
+# Todoを格納するリスト（仮のデータ）
+todos = [
+    {"id": 1, "title": "FastAPIを学ぶ", "completed": False},
+    {"id": 2, "title": "HTMXを理解する", "completed": False},
+    {"id": 3, "title": "Todoアプリを作る", "completed": True},
+]
 
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse(
-        "index.html", {"request": request, "title": "My Todo App"}
+        "index.html", {"request": request, "title": "My Todo App", "todos": todos}
     )
